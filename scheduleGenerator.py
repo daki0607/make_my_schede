e51c23043a2dc3155d783cfa704105d40817fa17
daki0607 <adityadangra@gmail.com>
2020-01-06 13:14:29 -0800

    ignore vscode files and successfully reads

--- scheduleGenerator.py
+++ scheduleGenerator.py
@@ -2,19 +2,27 @@ import json
 
 
 class Schedule(object):
-    def __init__(self, events_json):
-        pass
+    def __init__(self, filePointer):
+        data = json.load(filePointer)
+        self.events = []
+        for ev in data["events"]:
+            self.events.append(Event(ev))
 
 
 class Event(object):
-    def __init__(self, course, section, eventType, startTime, endTime, room):
-        self.course = course
-        self.section = section
-        self.eventType = eventType
-        self.startTime = to_hr_min(startTime)
-        self.endTime = to_hr_min(endTime)
-        self.room = room
+    def __init__(self, data):
+        self.course = data["course"]
+        self.section = data["section"]
+        self.eventType = data["type"]
+        self.startTime = to_hr_min(data["start"])
+        self.endTime = to_hr_min(data["end"])
+        self.room = data["room"]
 
 
 def to_hr_min(timeStr):
-    pass
+    hour, minute = timeStr.split(":")
+    return [int(hour), int(minute)]
+
+
+with open("schedule.json", "r") as F:
+    mySchedule = Schedule(F)
